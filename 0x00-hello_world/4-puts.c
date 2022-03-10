5498f8560b2c6ce12e6faa0ba6761a3195914a2f
Bismark <bismarkb609@gmail.com>
2022-03-10 15:11:59 -0800

    changed

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
-#include <stdio.h> 
-/* 
- *  printing a text 
+#include <stdio.h>
+/**
+*main -Entry point of the code 
+
+*Description: prints Programming is like building a multilingual puzzle
+
+* return: Alwasy 0 (success)
 */
-
-int main() {
-
-	puts("\"Programming is like building a multilingual puzzle\"");
-
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\"\n");
 	return (0);
-
 }
-
