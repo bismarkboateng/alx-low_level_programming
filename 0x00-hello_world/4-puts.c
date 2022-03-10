49385f40f3c06f5cf86a8d6f4aff465f9515aea9
Bismark <bismarkb609@gmail.com>
2022-03-10 13:59:31 -0800

    printing a message in c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 #include <stdio.h> 
 /* 
  *  printing a text 
@@ -7,7 +5,7 @@
 
 int main() {
 
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle\"");
 
 	return (0);
 
