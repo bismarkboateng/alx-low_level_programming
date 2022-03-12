ce40930befa119c03d59c54c609911ad482125b3
Bismark <bismarkb609@gmail.com>
2022-03-12 09:36:48 -0800

    all possible combinations of 1-9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+  * main - Prints a hexadecimal string
+  *
+  * Return: Always (Success)
+  */
+
+int main(void)
+{
+	int w;
+
+	for (w = '0'; w <= '9'; c++)
+	{
+		putchar(w);
+
+		if (w != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+
+	return (0);
+
+
+}
