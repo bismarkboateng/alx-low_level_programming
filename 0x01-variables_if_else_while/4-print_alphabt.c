15bdec67342ca0f81d597821da98fa7086309e86
Bismark <bismarkb609@gmail.com>
2022-03-12 08:20:29 -0800

    change

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,14 +9,20 @@
  */
 int main(void)
 {
-	for (int x = 'a'; x <= 'z'; x++)
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
 	{
+
 		if (x != 'e'  && x != 'q')
 		{
 			putchar(x);
 		}
 	}
+	
+
 	putchar('\n');
+
 
 	return (0);
 }
