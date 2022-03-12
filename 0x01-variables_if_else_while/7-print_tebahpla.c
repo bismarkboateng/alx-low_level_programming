ef0a780ad7d1c0d86d9280c1819a7054cb18b2d4
Bismark <bismarkb609@gmail.com>
2022-03-12 09:21:39 -0800

    Reverse num

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+  * main - print alphabet
+  *
+  * Return: Always (Success)
+  */
+int main(void)
+{
+	char x;
+
+	for (x = 'z'; x >= 'a'; x--)
+	{
+		putchar(x);
+	}
+
+	putchar('\n');
+
+
+	return (0);
+
+
+}
+
