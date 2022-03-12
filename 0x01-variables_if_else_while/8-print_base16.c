d724432591c84726856502d3863f23355e5e89e6
Bismark <bismarkb609@gmail.com>
2022-03-12 09:27:36 -0800

    prints a hexadecimal string

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+  * main - outputs a hexadecimal string
+  *
+  * Return: Always 0(Success)
+  */
+int main(void)
+{
+	char y;
+
+	for (y = '0'; y <= '9'; y++)
+	{
+		putchar(c);
+	}
+
+
+	for (y = 'a'; y <= 'f'; y++)
+	{
+		putchar(c);
+	}
+
+	putchar('\n');
+
+
+
+	return (0);
+}
