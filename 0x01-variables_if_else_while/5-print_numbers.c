89aa0d87a4fbe51e356b3849d2cdbdb6f9db443f
Bismark <bismarkb609@gmail.com>
2022-03-12 08:29:05 -0800

    numbers from zero to nine

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - prints numbers
+ *
+ * Description: numbers from zero to ten
+ *
+ * Return: Allways 0 (Success)
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0; x <= 9; x++)
+	{
+		printf("%d", x);
+	}
+
+	printf("\n");
+}
