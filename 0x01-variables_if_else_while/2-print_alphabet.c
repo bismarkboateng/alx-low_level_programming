d1db8f060b82c7dcfe0fcf631df837a82632759d
Bismark <bismarkb609@gmail.com>
2022-03-11 19:21:08 -0800

    printing alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - prints alphabets
+ *
+ * Description: prints lowercase alphabets
+ *
+ * Return: Allways 0 (Success)
+ */
+
+int main(void)
+{
+	for (int x = 'A'; x <= 'Z'; x++)
+	{
+		x = tolower(x);
+		putchar(x);
+		putchar("\n");
+	}
+
+	return (0);
+}
+
