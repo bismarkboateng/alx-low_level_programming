33e0cb6d73f87263e75ef9be91b3c71be4b25c5d
Bismark <60371335+bismarkb609@users.noreply.github.com>
2022-03-22 00:36:14 +0000

    a string followed by a new lin e

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - prints string
+ * @str: input string
+ * Return: no return
+ */
+
+void _puts(char *str)
+{
+         int i = 0;
+         while (str[i])
+         {
+                  _putchar(str[i]);
+                  i++;
+         }
+         _putchar('\n');
+}
