5d9a36996e7bc12ce3bc453046831610ab277b9e
Bismark <bismarkb609@gmail.com>
2022-03-21 18:17:05 -0700

    a string and a line

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -6,12 +6,13 @@
   *
   * Return: void
   */
+
 void _puts(char *str)
 {
-     for (; *str != '\0'; str++)
-     {
-	 _putchar(*str);
-     }
+	for (; *str != '\0'; str++)
+	{
+		_putchar(*str);
+	}
 
-     _putchar('\n');
+	_putchar('\n');
 }
