1ec0241e11e2f5024123695c7ef5b03f3df73e29
Bismark <bismarkb609@gmail.com>
2022-03-24 17:27:53 -0700

    concatenating strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,4 +1,18 @@
 #include "main.h"
 char *_strcat(char *dest, char *src){
-	return *dest + *src;
+	int i, n;
+
+	while (dest[i])
+	{
+		i++;
+	}
+	
+	for (n=0; src[n] != 0; i++)
+	{
+		dest[i] = src[n];
+		i++
+	}
+
+	dest[i] = '\0';
+	return (dest);
 }
