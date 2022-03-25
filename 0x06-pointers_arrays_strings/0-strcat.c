64a853228ca63491cca2e33ba9e1ba68593da9d4
Bismark <60371335+bismarkb609@users.noreply.github.com>
2022-03-25 00:57:27 +0000

    adding strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,18 +1,29 @@
 #include "main.h"
-char *_strcat(char *dest, char *src){
-	int i, n;
+#include <stdio.h>
 
-	while (dest[i])
-	{
-		i++;
-	}
+/**
+  * _strcat - Concatenates two strings
+  * @dest: The destination string
+  * @src: The source string
+  *
+  * Return: A pointer to the resulting string dest
+  */
+char *_strcat(char *dest, char *src)
+{
+   int dlen = 0, i;
+
+   while (dest[dlen])
+   {
+       dlen++;
+   }
+
+   for (i = 0; src[i] != 0; i++)
+   {
+       dest[dlen] = src[i];
+       dlen++;
+   }
+
+       dest[dlen] = '\0';
 	
-	for (n=0; src[n] != 0; i++)
-	{
-		dest[i] = src[n];
-		i++
-	}
-
-	dest[i] = '\0';
-	return (dest);
+       return (dest);
 }
